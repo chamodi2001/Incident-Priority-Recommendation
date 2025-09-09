60a0f267e89ae9b920b11709b2ae7c3dbca68df5
Gunathilaka Mastiyage Dona Chamodi <chamodigunathilaka2018@gmail.com>
2025-09-09 22:28:42 +0530

    updated incidents.js

--- incident-tracker-server/routes/incidents.js
+++ incident-tracker-server/routes/incidents.js
@@ -4,9 +4,43 @@ const db = require('../db');
 
 // POST /api/incidents - Create new incident
 router.post('/', (req, res) => {
-  const { title, severity, description, service } = req.body;
-  const sql = 'INSERT INTO incidents (title, severity, description, service) VALUES (?, ?, ?, ?)';
-  db.query(sql, [title, severity, description, service], (err, result) => {
+  const {
+    title,
+    service,
+    impactedSystem,
+    description,
+    affectedUsers,
+    affectedRegions,
+    highPriorityUsers,
+    highPriorityUserDetails,
+    businessImpact,
+    issueStartTime,
+    currentStatus,
+    workaround,
+    riskToOtherSystems,
+    dataImpact,
+    relatedToChange,
+    recurringIssue,
+    estimatedResolutionTime,
+    stakeholderNotification,
+    severity // Make sure your form includes this field!
+  } = req.body;
+
+  const sql = `
+    INSERT INTO incidents (
+      title, service, impactedSystem, description, affectedUsers, affectedRegions,
+      highPriorityUsers, highPriorityUserDetails, businessImpact, issueStartTime,
+      currentStatus, workaround, riskToOtherSystems, dataImpact, relatedToChange,
+      recurringIssue, estimatedResolutionTime, stakeholderNotification, severity
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql, [
+    title, service, impactedSystem, description, affectedUsers, affectedRegions,
+    highPriorityUsers, highPriorityUserDetails, businessImpact, issueStartTime,
+    currentStatus, workaround, riskToOtherSystems, dataImpact, relatedToChange,
+    recurringIssue, estimatedResolutionTime, stakeholderNotification, severity
+  ], (err, result) => {
     if (err) return res.status(500).send(err);
     res.status(201).json({ id: result.insertId, ...req.body });
   });
