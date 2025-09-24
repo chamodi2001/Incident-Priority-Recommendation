9efd944cfd3acc566807ccef6489f817c88b96c4
Gunathilaka Mastiyage Dona Chamodi <116811914+chamodi2001@users.noreply.github.com>
2025-09-24 21:38:01 +0530

    main.tf file changes

---
+++ terraform_disk_monitoring/main.tf
@@ -0,0 +1,34 @@
+resource "aws_sns_topic" "disk_alerts" {
+  name = "ec2-disk-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.disk_alerts.arn
+  protocol  = "email"
+  endpoint  = var.alarm_email
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_usage" {
+  alarm_name          = "ec2-root-disk-usage-${var.instance_id}"
+  alarm_description   = "Alarm when root (/) disk usage exceeds 75% on ${var.instance_id}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  period              = 1800           # 5 minutes
+  statistic           = "Average"     # or "Maximum" if you want spike-sensitive
+  threshold           = 75
+  namespace           = "CWAgent"
+  metric_name         = "disk_used_percent"
+
+  dimensions = {
+    InstanceId = var.instance_id
+    path       = "/"
+  }
+
+  alarm_actions = [
+    aws_sns_topic.disk_alerts.arn
+  ]
+
+  ok_actions = [
+    aws_sns_topic.disk_alerts.arn
+  ]
+}
